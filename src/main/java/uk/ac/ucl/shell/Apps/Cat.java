18501c4dadc66f9525d260cf346dcc5ee2e92ef3
hubamatyas <hubamatyas@gmail.com>
2022-11-24 19:07:49 +0000

    Refactor Cat.java

--- src/main/java/uk/ac/ucl/shell/Apps/Cat.java
+++ src/main/java/uk/ac/ucl/shell/Apps/Cat.java
@@ -12,29 +12,42 @@ import uk.ac.ucl.shell.Shell;
 
 public class Cat implements Application {
 
-    public void exec(ArrayList<String> args, InputStream input, OutputStreamWriter writer) {
-        if (args.isEmpty()) {
-            throw new RuntimeException("cat: missing arguments");
-        } else {
-            for (String arg : args) {
-                Charset encoding = StandardCharsets.UTF_8;
-                File currFile = new File(Shell.getDirectory() + File.separator + arg);
-                if (currFile.exists()) {
-                    Path filePath = Paths.get(Shell.getDirectory() + File.separator + arg);
-                    try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
-                        String line = null;
-                        while ((line = reader.readLine()) != null) {
-                            writer.write(String.valueOf(line));
-                            writer.write(System.getProperty("line.separator"));
-                            writer.flush();
-                        }
-                    } catch (IOException e) {
-                        throw new RuntimeException("cat: cannot open " + arg);
-                    }
-                } else {
-                    throw new RuntimeException("cat: file does not exist");
+    public void exec(ArrayList<String> args, InputStream input, OutputStreamWriter writer) throws IOException {
+        checkArgs(args);
+        enumerateArgs(args, writer);
+    }
+
+    private void enumerateArgs(ArrayList<String> args, OutputStreamWriter writer) {
+        for (String arg : args) {
+            Charset encoding = StandardCharsets.UTF_8;
+            String rawPath = Shell.getDirectory() + File.separator + arg;
+            File currFile = new File(rawPath);
+            if (!currFile.exists()) {
+                throw new RuntimeException("cat: file does not exist");
+            } else {
+                Path filePath = Paths.get(rawPath);
+                try (BufferedReader reader = Files.newBufferedReader(filePath, encoding)) {
+                    printFile(reader, writer);
+                }
+                catch (IOException e) {
+                    throw new RuntimeException("cat: cannot open " + arg);
                 }
             }
+        }
+    }
+
+    private void printFile(BufferedReader reader, OutputStreamWriter writer) throws IOException {
+        String line;
+        while ((line = reader.readLine()) != null) {
+            writer.write(line);
+            writer.write(System.getProperty("line.separator"));
+            writer.flush();
+        }
+    }
+
+    private void checkArgs(ArrayList<String> args) {
+        if (args.isEmpty()) {
+            throw new RuntimeException("cat: missing arguments");
         }
     }
 }
