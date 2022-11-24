108dfbb0fe7897500c5f4555306336df5e8006fc
hubamatyas <hubamatyas@gmail.com>
2022-11-24 20:07:00 +0000

    Minor refactor Echo.java

--- src/main/java/uk/ac/ucl/shell/Apps/Echo.java
+++ src/main/java/uk/ac/ucl/shell/Apps/Echo.java
@@ -8,15 +8,13 @@ import java.util.ArrayList;
 public class Echo implements Application{
 
     public void exec(ArrayList<String> args, InputStream input, OutputStreamWriter output) throws IOException {
-
-        boolean atLeastOnePrinted = false;
         for (String arg : args) {
             output.write(arg);
             output.write(" ");
             output.flush();
-            atLeastOnePrinted = true;
         }
-        if (atLeastOnePrinted) {
+
+        if (!args.isEmpty()) {
             output.write(System.getProperty("line.separator"));
             output.flush();
         }
