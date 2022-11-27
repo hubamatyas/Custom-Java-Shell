63bea85013384d499c46825dee72b48752e07940
Lucas <lucas.marrie.21@ucl.ac.uk>
2022-11-27 18:03:35 +0000

    fixed backquote related bug and added documentation to commands and parser

--- src/main/java/uk/ac/ucl/shell/Commands/Command.java
+++ src/main/java/uk/ac/ucl/shell/Commands/Command.java
@@ -8,10 +8,18 @@ public abstract class Command {
     
     private String inputString;
 
+    /**
+     * @param inputString : <code> String </code> to be evaluated
+     */
     public Command(String inputString){
         this.inputString = inputString;
     }
     
+    /**
+     * Evaluates the <code>inputString</code> that was passed into the constructor
+     * @param input : nullable <code> InputStream </code> to substitute missing file argument, primarly used for piping
+     * @param output : <code> OutputStream </code> to write the evaluation of the command to
+     */
     public abstract void eval(InputStream input, OutputStream output) throws IOException;
 
     protected String getInputString(){
