1c857af17813cecc1667ffb58199ee59ee72b240
hubamatyas <hubamatyas@gmail.com>
2022-11-28 15:22:55 +0000

    Handle piping in Cd

--- src/main/java/uk/ac/ucl/shell/Apps/Cd.java
+++ src/main/java/uk/ac/ucl/shell/Apps/Cd.java
@@ -1,11 +1,9 @@
 package uk.ac.ucl.shell.Apps;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import uk.ac.ucl.shell.Shell;
 
 public class Cd extends Application {
     public Cd(ArrayList<String> args, InputStream input, OutputStreamWriter writer) {
@@ -13,18 +11,18 @@ public class Cd extends Application {
     }
 
     @Override
+    protected void checkArgs() {
+        if (args.isEmpty()) {
+            throw new RuntimeException("cd: missing argument");
+        } else if (args.size() > 1 || input != null) {
+            throw new RuntimeException("cd: too many arguments");
+        }
+    }
+
+    @Override
     protected void eval() throws IOException {
         String subDir = args.get(0);
         directory.checkDirectoryToHandle("cd", subDir);
         directory.setCurrentDirectory(String.valueOf(directory.getPathTo(subDir)));
-    }
-
-    @Override
-    protected void checkArgs() {
-        if (args.isEmpty()) {
-            throw new RuntimeException("cd: missing argument");
-        } else if (args.size() > 1) {
-            throw new RuntimeException("cd: too many arguments");
-        }
     }
 }
