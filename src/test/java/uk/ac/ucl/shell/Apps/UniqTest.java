5df90c710997358bbcd9b6a6a9cca83d087f6559
matthewgomezcullen <zcabgom@ucl.ac.uk>
2022-11-30 15:44:36 +0000

    added uniq tests

--- src/test/java/uk/ac/ucl/shell/Apps/UniqTest.java
+++ src/test/java/uk/ac/ucl/shell/Apps/UniqTest.java
@@ -1,7 +1,47 @@
 package uk.ac.ucl.shell.Apps;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
-public class UniqTest {
+public class UniqTest extends OutputTest{
+
+    String fileName;
+    String content;
+
+    @Override
+    public String setAppName() {
+        return "uniq";
+    }
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        fileName = "uniqTest";
+        String[] lines = new String[]{"Zzz", "abba", "Abba", "zzz", "zzz", "Zzz", "abba", "abba"};
+        content = buildContent(lines);
+
+        generateTestFile(fileName, content);
+    }
+
+    private String buildContent(String[] lines) {
+        StringBuilder contentBuilder = new StringBuilder();
+        for (String line : lines) {
+            contentBuilder.append(line).append(lineSeparator);
+        }
+        return contentBuilder.toString();
+    }
+
+    // Functionality
+    @Test
+    public void uniqTest() throws IOException {
+        String expectedOutput = buildContent(new String[]{"Zzz", "abba", "Abba", "zzz", "Zzz", "abba"});
+        testOutput(createArgs(fileName), null, expectedOutput);
+    }
+
 
 }
