e221b9d8921b508aa5add33ff71d829b630d5f3c
hubamatyas <hubamatyas@gmail.com>
2022-11-30 14:21:23 +0000

    Fixed test_cut and test_cut_stdin bug

--- src/main/java/uk/ac/ucl/shell/Apps/Cut.java
+++ src/main/java/uk/ac/ucl/shell/Apps/Cut.java
@@ -124,19 +124,18 @@ public class Cut extends Application {
 
     private String notToEOL(StringBuilder sb, String line) {
         for (int i : this.bytesRange) {
-            if (i <= line.length() && i > 1) {
+            if (i <= line.length() && i > 0) {
                 sb.append(line.charAt(i-1));
             }
         }
         return sb.toString();
     }
 
-    private StringBuilder notToEOL(StringBuilder sb, String line, int end) {
+    private void notToEOL(StringBuilder sb, String line, int end) {
         for (int i : this.bytesRange) {
             if (i <= line.length() && i > 0 && i < end) {
                 sb.append(line.charAt(i-1));
             }
         }
-        return sb;
     }
 }
